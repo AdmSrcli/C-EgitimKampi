3cc9e65cdc2a51684e0930ea26651988f652a49a
Adem SARAÇLI <adem.saracli@hotmail.com>
2024-10-22 14:35:57 +0300

    C# Egitim Kampi

---
+++ CSharpEgitimKampi/09_Database Project/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Database_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("***** C# Veri Tabanlı ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen Getirmek İstediğiniz Tablo Numarasını Giriniz:    ");
+            tableNumber= Console.ReadLine();
+            Console.WriteLine("********************************************************");
+
+            SqlConnection connection = new SqlConnection("Data Source=MA-JGVZR14;Initial Catalog=EgitimKampiDb;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select*from TblCategory",connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+
+            foreach(DataRow row in dt.Rows)
+            {
+                foreach(var Items in row.ItemArray)
+                {
+                    Console.Write(Items.ToString());
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
---
+++ CSharpEgitimKampi/09_Database Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("09_Database Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johnson Controls International, PLC")]
+[assembly: AssemblyProduct("09_Database Project")]
+[assembly: AssemblyCopyright("Copyright © Johnson Controls International, PLC 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00df6a37-3cad-450b-81fa-d67f8477785e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
